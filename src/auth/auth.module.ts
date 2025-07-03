4501f1a3f4bb2cb27f4e44e022f08d7e85170621
sajib-sv <sajib.softvence@gmail.com>
2025-07-04 01:50:23 +0600

    fix: fixed auth module dependency

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaService } from '@project/prisma/prisma.service';
+import { JwtStrategy } from '@project/common/jwt/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ENVEnum } from '@project/common/enum/env.enum';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    ConfigModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        secret: await config.get(ENVEnum.JWT_SECRET),
+        signOptions: {
+          expiresIn: await config.get(ENVEnum.JWT_EXPIRES_IN),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService, JwtStrategy],
 })
 export class AuthModule {}
